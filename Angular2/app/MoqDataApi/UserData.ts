0c74f6c9d1675553916ed638aa5f283add148085
Paul Swetz <pswetz@users.noreply.github.com>
2016-09-29 10:19:01 -0400

    Update UserData.ts

--- Angular2/app/MoqDataApi/UserData.ts
+++ Angular2/app/MoqDataApi/UserData.ts
@@ -4,7 +4,7 @@ export class UserData implements InMemoryDbService {
     public static Url:string = "/MoqDataApi/users";
     public createDb() {
         let users: User[] = [
-            new User(1, "pswetz", "Paul", "Swetz", "pswetz@gmail.com"),
+            new User(1, "user1", "1", "lname1", "1@gmail.com"),
             new User(2, "user2", "2", "laname2", "2@gmail.com"),
             new User(3, "user3", "3", "laname3", "3@gmail.com"),
             new User(4, "user4", "4", "laname4", "4@gmail.com")
