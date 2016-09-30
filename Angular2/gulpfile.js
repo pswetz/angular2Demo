ca293fe9ee22db90b56f0d0eea52e015974cea2a
Paul Swetz <Randomseed@Pauls-MacBook-Pro.local>
2016-09-30 18:51:42 -0400

    trying to get gulp-typescript to output correctly

--- Angular2/gulpfile.js
+++ Angular2/gulpfile.js
@@ -4,7 +4,9 @@ var gulp = require("gulp");
 var plugins = {
     rm: require("gulp-rm"),
     typings: require("gulp-typings"),
-    webServer: require('gulp-webserver')
+    webServer: require('gulp-webserver'),
+    typescript: require('gulp-typescript'),
+    debug: require('gulp-debug')
 };
 
 
@@ -22,11 +24,21 @@ gulp.task('clean:tmp', function () {
 });
 
 gulp.task('typings:install', function () {
-    return gulp.src("./typings.json")
+    return gulp.src("typings.json" )
         .pipe(plugins.typings());
 });
+var tsProject = plugins.typescript.createProject('tsconfig.json');
+gulp.task('build',function(){
+    var tsResult = tsProject.src() // instead of gulp.src(...) 
+        .pipe(plugins.debug())
+        .pipe(tsProject());
+ 
+    return tsResult.js.pipe(gulp.dest('app'));
+});
+gulp.task('run',['build','webserver'],function(){
 
-gulp.task('server', function() {
+});
+gulp.task('webserver', function() {
     gulp.src('')
     .pipe(plugins.webServer({
       livereload: true,
